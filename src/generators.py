2ea4054464fd825a2f9c8b5026108594159470ef
Oleg Skripnikov <osv719@gmail.com>
2025-04-17 17:28:39 +0700

    develop filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -2,7 +2,12 @@ from typing import Any
 
 
 def filter_by_currency(transactions: list, currency_code: str) -> Any:
-    pass
+    """Функция фильтрации списка словарей по заданной валюте (возвращает итератор)"""
+    for transaction in transactions:  # Перебираем словари
+        operation_amount = transaction.get("operationAmount", {})  # Заходим в нужный нам ключ
+        currency = operation_amount.get("currency", {})
+        if currency.get("code") == currency_code:
+            yield transaction
 
 
 def transaction_descriptions(transactions: list) -> Any:
