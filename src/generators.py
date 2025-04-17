c5d5ea9b3d2b8a20a711ffa6e3678d4dd0d8e695
Oleg Skripnikov <osv719@gmail.com>
2025-04-17 17:22:58 +0700

    add generators and test_generators

---
+++ src/generators.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+
+def filter_by_currency(transactions: list, currency_code: str) -> Any:
+    pass
+
+
+def transaction_descriptions(transactions: list) -> Any:
+    pass
+
+
+def card_number_generator(start: Any, end: int) -> Any:
+    pass
