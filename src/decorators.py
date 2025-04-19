50ebec166d53e98a9aa9b3263fbe6871e57582d7
Oleg Skripnikov <osv719@gmail.com>
2025-04-19 16:16:40 +0700

    add log to file and test it(+html.cov)

--- src/decorators.py
+++ src/decorators.py
@@ -18,6 +18,7 @@ from typing import Any
 
 def log(filename: Any = None) -> Any:
     """Декоратор для логирования выполнения функции в консоль или в файл(опция)"""
+
     def decorator(func: Any) -> Any:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -26,16 +27,24 @@ def log(filename: Any = None) -> Any:
             inputs = f"Inputs: {args}, {kwargs}"
             # Формируем сообщение о начале выполнения функции
             start_time = datetime.now()
-            start_msg = f"{start_time} - {func_name} started. {inputs}"
-            print(start_msg)
+            start_msg = f"{start_time} - {func_name} started. {inputs}\n"
+            if filename:
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(start_msg)
+            else:
+                print(start_msg, end="")
             try:
                 # Выполняем функцию
                 result = func(*args, **kwargs)
                 end_time = datetime.now()
                 time_execution = end_time - start_time
                 # Формируем сообщение об успешном выполнения функции
-                success_msg = f"{end_time} - {func_name} OK. Result: {result}. Time execution: {time_execution}"
-                print(success_msg)
+                success_msg = f"{end_time} - {func_name} OK. Result: {result}. Time execution: {time_execution}\n"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(success_msg)
+                else:
+                    print(success_msg, end="")
                 return result
             except Exception as exceptions:
                 end_time = datetime.now()
@@ -43,10 +52,13 @@ def log(filename: Any = None) -> Any:
                 # Формируем сообщение об ошибке выполнения функции
                 error_msg = (
                     f"{end_time} - {func_name} error: {type(exceptions).__name__}: {str(exceptions)}."
-                    f" {inputs}. Time execution: {time_execution}"
+                    f" {inputs}. Time execution: {time_execution}\n"
                 )
-
-                print(error_msg)
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(error_msg)
+                else:
+                    print(error_msg, end="")
                 raise  # Передаём пойманное исключение дальше
 
         return wrapper
@@ -54,10 +66,9 @@ def log(filename: Any = None) -> Any:
     return decorator
 
 
-@log()
-def my_function(x: Any, y: Any) -> Any:
-    """Функция проверки декоратора"""
-    return x / y
-
-
+# @log(filename="../mylog.txt")
+# def my_function(x: Any, y: Any) -> Any:
+#     """Функция проверки декоратора"""
+#     return x / y
+#
 # my_function(1, 2)
--- tests/test_log.py
+++ tests/test_log.py
@@ -68,3 +68,77 @@ def test_log_decorator_error_type(capsys: Any) -> Any:
         )
         captured = capsys.readouterr()
         assert "my_function() missing 1 required positional argument: 'y'" in captured.out
+
+
+# 6) Тестируем вывод в файл (.txt) логов (входные параметры декорируемой функции (1, 2)).
+def test_file_logging_1() -> Any:
+    test_file = "tests/test_file.txt"  # Прописываем создаваемый файл и его путь
+    if os.path.exists(test_file):  # Проверяем существование такого файла
+        os.remove(test_file)  # Если есть, удаляем, чтобы создался заново (т.к. в декораторе режим добавления 'a')
+
+    @log(filename=test_file)  # Определяем функцию с файлом внутри теста.
+    def my_function(x: Any, y: Any) -> Any:
+        return x / y
+
+    my_function(1, 2)
+    with open(test_file, "r") as file:  # Открываем файл в режиме чтения
+        content = file.read()  # Сохраняем данные из файла в переменную
+    assert "- my_function started. Inputs: (1, 2), {}" in content  # Сравниваем содержимое с ожиданием
+    assert "- my_function OK. Result: 0.5. Time execution:" in content
+    os.remove(test_file)  # Удаляем тестовый файл после использования
+
+
+# 7) Тестируем вывод в файл (.txt) логов (входные параметры декорируемой функции (20, 20)).
+def test_file_logging_2() -> Any:
+    test_file = "tests/test_file.txt"
+    if os.path.exists(test_file):
+        os.remove(test_file)
+
+    @log(filename=test_file)
+    def my_function(x: Any, y: Any) -> Any:
+        return x / y
+
+    my_function(20, 20)
+    with open(test_file, "r") as file:
+        content = file.read()
+    assert "- my_function started. Inputs: (20, 20), {}" in content
+    assert "- my_function OK. Result: 1.0. Time execution:" in content
+    os.remove(test_file)
+
+
+# 8) Тестируем вывод в файл (.txt) логов об ошибке 'ZeroDivisionError'(входные параметры декорируемой функции (1, 0)).
+def test_file_logging_error_ZeroDiv() -> Any:
+    test_file = "tests/test_file_error.txt"
+    if os.path.exists(test_file):
+        os.remove(test_file)
+
+    @log(filename=test_file)
+    def my_function(x: Any, y: Any) -> Any:
+        return x / y
+
+    with pytest.raises(ZeroDivisionError, match="division by zero"):
+        my_function(1, 0)
+        with open(test_file, "r") as file:
+            content = file.read()
+        assert "- my_function error: ZeroDivisionError: division by zero. Inputs: (1, 0), {}." in content
+    os.remove(test_file)
+
+
+# 9) Тестируем вывод в (.txt) логов об ошибке 'TypeError'(входные параметры декорируемой функции (1, )).
+def test_file_logging_error_type() -> Any:
+    test_file = "tests/test_file_error.txt"
+    if os.path.exists(test_file):
+        os.remove(test_file)
+
+    @log(filename=test_file)
+    def my_function(x: Any, y: Any) -> Any:
+        return x / y
+
+    with pytest.raises(TypeError):
+        my_function(
+            1,
+        )
+        with open(test_file, "r") as file:
+            content = file.read()
+        assert 'my_function() missing 1 required positional argument: "y"' in content
+    os.remove(test_file)
