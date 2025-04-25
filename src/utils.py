d0b68549618b7d04b7a054ec6092f5a80e42b0e4
Oleg Skripnikov <osv719@gmail.com>
2025-04-25 08:52:00 +0700

    develope load_transactions_list

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,25 @@
+import json
+# from typing import Any
+
+
 def load_transactions_list(file_name: str) -> list:
     """
     Функция принимает на вход (file_name) путь до JSON-файла и
      возвращает список словарей с данными о финансовых транзакциях.
      Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
-    pass
+    try:
+        with open(file_name, "r", encoding="utf-8") as f:  # Обращаемся к файлу по заданному пути
+            data = json.load(f)  # Преобразуем JSON-файл в файл Python
+            if isinstance(data, list):  # Проверка на соответствие типа данных
+                return data  # Если соответствует, возвращаем список словарей
+            return []  # В обратном случае возвращаем пустой список
+    except (FileNotFoundError, json.JSONDecodeError):  # Если не найден, или не список возвращаем []
+        return []
+
+
+# file_names = "../data/operations.json"
+#
+# result = load_transactions_list(file_names)
+#
+# print(result[0])
