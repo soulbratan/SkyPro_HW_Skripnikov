50f0373b45b9dbbe7ade9ae639e44e395844ed89
Олег Скрипников <oleg-skripnikov8@mail.ru>
2025-05-05 13:30:41 +0700

    add search_transaction()

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 # Новый модуль для функций обработки данных
+import re
 
 
 def filter_by_state(unfiltered_list: list, filtering_state: str = "EXECUTED") -> list:
@@ -20,9 +21,61 @@ def sort_by_date(unsorted_list: list, reverse: bool = True) -> list:
     )  # Возвращаем отсортированный по дате список с помощью sorted
 
 
+def search_transactions(transactions: list[dict], search_text: str) -> list[dict]:
+    """Функция принимает список словарей с транзакциями и строку поиска. Возвращает отфильтрованный список словарей"""
+    new_list = list()
+    pattern = f"{search_text}"
+    for i in transactions:
+        clean_text = re.sub(r"[^\w\s]", "", str(i), flags=re.UNICODE)
+        search_result = re.search(pattern, clean_text, flags=re.IGNORECASE)
+        if search_result:
+            new_list.append(i)
+    return new_list
+
+
 test_list = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589",
+    },
+    {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560",
+    },
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 587085106,
+        "state": "EXECUTED",
+        "date": "2018-03-23T10:45:06.972075",
+        "operationAmount": {"amount": "48223.05", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Открытие вклада",
+        "to": "Счет 41421565395219882431",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
 ]
