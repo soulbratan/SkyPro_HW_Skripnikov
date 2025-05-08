0955c101b6756876f871515bd1b0e1861a862ad6
Олег Скрипников <oleg-skripnikov8@mail.ru>
2025-05-08 12:12:36 +0700

    add try-except in generators and main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from src import utils
-from src import read_tables
-from src import processing
-from src import generators
-from src import widget
+from src import generators, processing, read_tables, utils, widget
+
 
 def main():
     print(
@@ -12,45 +9,47 @@ def main():
         "2. Получить информацию о транзакциях из CSV-файла\n"
         "3. Получить информацию о транзакциях из XLSX-файла"
     )
-    choice_1 = input("Ваш выбор: ") # Выбираем откуда взять информацию о транзакциях
+    choice_1 = input("Ваш выбор: ")  # Выбираем откуда взять информацию о транзакциях
     if choice_1 == "1":
         print("Для обработки выбран JSON-файл.")
-        result_1 = utils.load_transactions_list("data/operations.json") # Транзакции из JSON-файла
+        result_1 = utils.load_transactions_list("data/operations.json")  # Транзакции из JSON-файла
     elif choice_1 == "2":
         print("Для обработки выбран CSV-файл.")
-        result_1 = read_tables.csv_to_dict("data/transactions.csv") # Транзакции из CSV-файла
+        result_1 = read_tables.csv_to_dict("data/transactions.csv")  # Транзакции из CSV-файла
     elif choice_1 == "3":
         print("Для обработки выбран XLSX-файл.")
-        result_1 = read_tables.excel_to_dicts("data/transactions_excel.xlsx") # Транзакции из XLSX-файла
+        result_1 = read_tables.excel_to_dicts("data/transactions_excel.xlsx")  # Транзакции из XLSX-файла
     else:
         print("Вы не выбрали ничего из предложенного списка")
-        return [{}] # Пустые данные
-    correct = 0 # Для цикла while
-    while correct == 0: # Запускаем цикл, пока пользователь не выберет правильно
-        print("Введите статус, по которому необходимо выполнить фильтрацию. "
-              "Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING")
+        return [{}]  # Пустые данные
+    correct = 0  # Для цикла while
+    while correct == 0:  # Запускаем цикл, пока пользователь не выберет правильно
+        print(
+            "Введите статус, по которому необходимо выполнить фильтрацию. "
+            "Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING"
+        )
         choice_2 = input("Ваш выбор: ").upper()
         if choice_2 == "EXECUTED" or choice_2 == "CANCELED" or choice_2 == "PENDING":
             print(f"Операции отфильтрованы по статусу {choice_2}")
-            result_2 = processing.filter_by_state(result_1, choice_2) # Фильтруем по статусу
+            result_2 = processing.filter_by_state(result_1, choice_2)  # Фильтруем по статусу
             correct = 1
         else:
-            print(f"Статус операции {choice_2} недоступен.") # Если некорректный ввод, то заново
+            print(f"Статус операции {choice_2} недоступен.")  # Если некорректный ввод, то заново
     print("Отсортировать операции по дате? Да/Нет")
     choice_3 = input("Ваш выбор: ").lower()
     if choice_3 == "да":
         print("Отсортировать 'по возрастанию' или 'по убыванию'?")
         choice_31 = input("Ваш выбор: ").lower()
         if choice_31 == "по возрастанию":
-            result_3 = processing.sort_by_date(result_2, False) # Сортируем по возрастанию
+            result_3 = processing.sort_by_date(result_2, False)  # Сортируем по возрастанию
         elif choice_31 == "по убыванию":
-            result_3 = processing.sort_by_date(result_2) # Сортируем по убыванию
+            result_3 = processing.sort_by_date(result_2)  # Сортируем по убыванию
     else:
         result_3 = result_2
     print("Выводить только рублевые транзакции? Да/Нет")
     choice_4 = input("Ваш выбор: ").lower()
     if choice_4 == "да":
-        result_4 = [i for i in generators.filter_by_currency(result_3, "RUB")] # Фильтруем по рублёвой валюте
+        result_4 = [i for i in generators.filter_by_currency(result_3, "RUB")]  # Фильтруем по рублёвой валюте
     else:
         result_4 = result_3
     print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
@@ -60,20 +59,26 @@ def main():
         result_5 = processing.search_transactions(result_4, choice_51)
     else:
         result_5 = result_4
-    print("Распечатываю итоговый список транзакций...")
+    print("Распечатываю итоговый список транзакций...\n")
     if len(result_5) == 0:
         print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
     else:
-        print(f"Всего банковских операций в выборке: {len(result_5)}")
+        print(f"Всего банковских операций в выборке: {len(result_5)}\n")
         for i in result_5:
             first_line = f"{widget.get_date(i['date'])} {i.get('description')}\n"
-            if i.get('description').lower() == "открытие вклада":
+            if i.get("description").lower() == "открытие вклада":
                 second_line = f"{widget.mask_account_card(i.get('to'))}\n"
             else:
-                second_line = f"{widget.mask_account_card(i.get('from', "Новый"))} -> {widget.mask_account_card(i.get('to'))}\n"
-            third_line = f"Сумма: {i.get('operationAmount').get('amount')} {i.get('operationAmount').get('currency').get('name')}\n"
-        print(f"{first_line}{second_line}{third_line}")
+                second_line = (
+                    f"{widget.mask_account_card(i.get('from', "Новый"))} -> {widget.mask_account_card(i.get('to'))}\n"
+                )
+            try:
+                third_line = (
+                    f"Сумма: {i['operationAmount'].get('amount')} {i['operationAmount'].get('currency').get('name')}\n"
+                )
+            except KeyError:
+                third_line = f"Сумма: {i.get('amount')} {i.get('currency_name')}\n"
+            print(f"{first_line}{second_line}{third_line}")
+
 
 x = main()
-
-
--- src/generators.py
+++ src/generators.py
@@ -4,10 +4,14 @@ from typing import Any
 def filter_by_currency(transactions: list, currency_code: str) -> Any:
     """Функция фильтрации списка словарей по заданной валюте (возвращает итератор)"""
     for transaction in transactions:  # Перебираем словари
-        operation_amount = transaction.get("operationAmount", {})  # Заходим в нужный нам ключ
-        currency = operation_amount.get("currency", {})
-        if currency.get("code") == currency_code:
-            yield transaction
+        try:
+            operation_amount = transaction["operationAmount"]  # Заходим в нужный нам ключ
+            currency = operation_amount.get("currency", {})
+            if currency.get("code") == currency_code:
+                yield transaction
+        except KeyError:
+            if transaction.get("currency_code", {}) == currency_code:
+                yield transaction
 
 
 def transaction_descriptions(transactions: list) -> Any:
